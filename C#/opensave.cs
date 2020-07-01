ad38d4a16fc9b1f76e055677d69d5bd870e535ec
J.B Stepan <tonymoooon543@gmail.com>
2020-06-30 23:28:03 -0500

    Add files via upload

---
+++ C#/opensave.cs
@@ -0,0 +1,53 @@
+using system;
+
+namespace saveandopen
+{
+  // Makes the function for opening files
+        private void Openfile()
+        {
+            // From Microsoft, so all rights go to them
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+                // code here
+            }
+
+            
+        }
+
+        // Makes the function for saving files
+        private void SaveFile()
+        {
+            // From Microsoft, so all rights go to them
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Code here
+            }
+        }
+}
